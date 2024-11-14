b0c465b0a49dec6bd894c7d2742dbed7224d79b1
Nabeeha Rehman <nabeeharehmanali3@gmail.com>
2024-11-14 17:52:13 +0500

    final nabeeha

--- game.h
+++ game.h
@@ -16,8 +16,8 @@ class Enemy{
     private:
         int DamageAmount;
     public:
-    Enemy():DamageAmount(0){}
-    Enemy(int damage):DamageAmount(damage){}
+    Enemy();
+    Enemy(int damage);
 
 };
 
@@ -25,16 +25,16 @@ class Spider: public Enemy{
     private:
     int acid_balls;
     public:
-    Spider():Enemy(0),acid_balls(0){}
-    Spider(int damage,int balls):Enemy(d),acid_balls(balls){}
+    Spider();
+    Spider(int damage,int balls);
 
 };
 
 class Bats:public Enemy{
     private:
     public:
-    Bats():Enemy(0){}
-    Bats(int damage):Enemy(damage){}
+    Bats();
+    Bats(int damage);
 
 };
 
